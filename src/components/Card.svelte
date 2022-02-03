ea567ec0dec6269929a9f9d98a98b632aabe941c
Raafat <raafat.basheer@adorsys.com>
2022-02-03 16:54:04 +0100

    init commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let className = "";
+</script>
+
+<div class="card {className}">
+  
+    <slot></slot>
+  </div>
+  
+  <style>
+    .card {
+      background-color: #fff;
+      color: #333;
+      border-radius: 15px;
+      padding: 40px 50px;
+      margin: 20px 0;
+      position: relative;
+    }
+    .card-list:hover {
+      box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+      transform: scale(1.01);
+    }
+  </style>
---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,47 @@
+<script>
+    import {FeedbackStore} from '../stores'
+    import Card from './Card.svelte'
+    export let item
+  
+    const handleDelete = (itemId) => {
+      FeedbackStore.update((currentFeedback) => {
+        return currentFeedback.filter(item => item.id != itemId)
+      })
+    }
+  </script>
+  
+  <Card className="card-list">
+    <div class="num-display">
+      {item.rating}
+    </div>
+    <button class="close" on:click={() => handleDelete(item.id)}>X</button>
+    <p class="text-display">
+      {item.text}
+    </p>
+  </Card>
+  
+  <style>
+    .num-display {
+      position: absolute;
+      top: -5px;
+      left: -5px;
+      width: 50px;
+      height: 50px;
+      background: #5476ff;
+      color: #fff;
+      border: 1px #eee solid;
+      border-radius: 50%;
+      padding: 10px;
+      text-align: center;
+      font-size: 19px;
+    }
+  
+    .close {
+      position: absolute;
+      top: 10px;
+      right: 20px;
+      cursor: pointer;
+      background: none;
+      border: none;
+    }
+  </style>
